2dc9aac31cb05d42054628b599ac25082caeb854
janitra <jajajanitra@gmail.com>
2020-12-28 00:39:24 +0700

    update

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,4 +1,10 @@
 export const CourseCard = (props) => {
   // TODO: design HTML component that displays a course as a "card" on the webpage.
-  return (null);
+  return <>
+  
+  <p>- Course name : {props.name}</p>
+  <p>- Credit : {props.crd}</p>
+  <p>- Grade : {props.grd}</p>
+  <button onClick = {() =>props.del(props.name)} > --- Delete this course --- </button><br></br><br></br>
+  </>;
 };
